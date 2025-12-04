5b2c886300138d2123e9bc044aecefde927ea9b3
Keanu Heydenrych <keanuh98@gmail.com>
2025-12-04 12:55:02 +0200

    Fourth

--- Exercise.go
+++ Exercise.go
@@ -1,4 +1,4 @@
-package Exercise
+package main
 
 import (
 	"fmt"
@@ -8,55 +8,49 @@ import (
 
 func main() {
 
-	/*
-		fmt.Println("Hello")
-		fmt.Println(`This
+	fmt.Println("Hello")
+	fmt.Println(`This
 			is
 			a
 			`)
 
-		a := 42
-		fmt.Println(a)
+	a := 42
+	fmt.Println(a)
 
-		b, c, d, _, f1 := 0, 1, 2, 3, "Happiness"
-		fmt.Println(b, c, d, f1)
+	b, c, d, _, f1 := 0, 1, 2, 3, "Happiness"
+	fmt.Println(b, c, d, f1)
 
-		var g int
-		fmt.Println(g)
+	var g int
+	fmt.Println(g)
 
-		adams := 42
-		fmt.Printf("42 as binary, %b \n", adams)
-		fmt.Printf("42 as hexadecimal, %x \n", adams)
-	*/
+	adams := 42
+	fmt.Printf("42 as binary, %b \n", adams)
+	fmt.Printf("42 as hexadecimal, %x \n", adams)
 
-	/*
-		a, b, c, d = 0, 1, 2, 3
-		e, f := 4, 5
+	a, b, c, d = 0, 1, 2, 3
+	e := 4
+	f := 5
 
-		fmt.Printf("%v \t %b \t %X \n", a, a, a)
-		fmt.Printf("%v \t %b \t %X \n", b, b, b)
-		fmt.Printf("%v \t %b \t %X \n", c, c, c)
-		fmt.Printf("%v \t %b \t %X \n", d, d, d)
-		fmt.Printf("%v \t %b \t %X \n", e, e, e)
-		fmt.Printf("%v \t %b \t %X \n", f, f, f)
+	fmt.Printf("%v \t %b \t %X \n", a, a, a)
+	fmt.Printf("%v \t %b \t %X \n", b, b, b)
+	fmt.Printf("%v \t %b \t %X \n", c, c, c)
+	fmt.Printf("%v \t %b \t %X \n", d, d, d)
+	fmt.Printf("%v \t %b \t %X \n", e, e, e)
+	fmt.Printf("%v \t %b \t %X \n", f, f, f)
 
-	*/
+	y := 42
+	z := 42.0
+	fmt.Printf("%v of type %T \n", y, y)
+	fmt.Printf("%v of type %T \n", z, z)
 
-	/*
-		y := 42
-		z := 42.0
-		fmt.Printf("%v of type %T \n", y, y)
-		fmt.Printf("%v of type %T \n", z, z)
+	var m float32 = 43.742
+	fmt.Printf("%v of type %T \n", m, m)
 
-		var m float32 = 43.742
-		fmt.Printf("%v of type %T \n", m, m)
+	//z = m
+	//fmt.Printf("%v of type %T \n", z, z)
 
-		// z = m
-		// fmt.Printf("%v of type %T \n", z, z)
-
-		z = float64(m)
-		fmt.Printf("%v of type %T \n", z, z)
-	*/
+	z = float64(m)
+	fmt.Printf("%v of type %T \n", z, z)
 
 	// Exercise 2
 
@@ -69,43 +63,43 @@ func main() {
 	pi := math.Pi
 	fmt.Println("Pi", pi)
 
-	fmt.Println("Addition functon:", add(4, 3))
+	fmt.Println("Addition functon:", Add(4, 3))
 
-	fmt.Println("Multiplaction function", multi(20, 5))
+	fmt.Println("Multiplaction function", Multi(20, 5))
 
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	a1, b1 := Swap("hello", "world")
+	fmt.Println(a1, b1)
 
-	x, y := split(17)
+	x := 0
+	x, y = Split(17)
 
 	fmt.Println("Split ", x, y)
 
-	fmt.Println("NeedInt ", needInt(Small))
-	fmt.Println("needFloat small", needFloat(Small))
-	fmt.Println("needFloat Big", needFloat(Big))
+	fmt.Println("NeedInt ", NeedInt(Small))
+	fmt.Println("needFloat small", NeedFloat(Small))
+	fmt.Println("needFloat Big", NeedFloat(Big))
 
-	/*
-		fmt.Println(y) // Var for zero value
+	fmt.Println(y) // Var for zero value
 
-		z := 10 // Short declaration operator
-		fmt.Println("Declaration ", z)
+	z = 10 // Short declaration operator
+	fmt.Println("Declaration ", z)
 
-		a, b, c := 2, 4, 6 // Multiple initializations
-		fmt.Println("Multiple", a, b, c)
+	a, b, c = 2, 4, 6 // Multiple initializations
+	fmt.Println("Multiple", a, b, c)
 
-		var y float64 = 100.01 // Var when specificity is required
-		fmt.Println("specific", y)
+	var y1 float64 = 100.01 // Var when specificity is required
+	fmt.Println("specific", y1)
 
-		_ = 20 // Blank indentifier
+	_ = 20 // Blank indentifier
 
-	*/
+	a, b, c = 747, 911, 90210
 
-	/*
-		a, b, c := 747, 911, 90210
+	fmt.Printf("%d \t %b \t %#X \n", a, a, a)
+	fmt.Printf("%d \t %b \t %#X \n", b, b, b)
+	fmt.Printf("%d \t %b \t %#X \n", c, c, c)
 
-		fmt.Printf("%d \t %b \t %#X \n", a, a, a)
-		fmt.Printf("%d \t %b \t %#X \n", b, b, b)
-		fmt.Printf("%d \t %b \t %#X \n", c, c, c)
-	*/
+	fmt.Println(Bark())
+
+	fmt.Println("Intern2 code", functions2.smallBark())
 
 }
--- functions.go
+++ functions.go
@@ -1,9 +1,39 @@
-package Intern
+package main
 
-func Bark() string {
-	return "Woof!"
+func Add(x int, y int) int {
+
+	return x + y
 }
 
-func Barks() string {
-	return "Woof Woof Woof"
+func Multi(x int, y int) int {
+
+	return x * y
+}
+
+func Swap(x, y string) (string, string) {
+
+	return y, x
+}
+
+func Split(sum int) (x, y int) {
+
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
+
+func NeedInt(x int) int {
+	return x*10 + 1
+}
+func NeedFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func Bark() string {
+	return "WOOOOF!"
 }
