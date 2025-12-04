dd2034c5780c3dc5ef9bddafdeb0d7da93775cae
Keanu Heydenrych <keanuh98@gmail.com>
2025-12-04 15:27:52 +0200

    third

--- New/main.go
+++ New/main.go
@@ -7,3 +7,15 @@ func main() {
 	fmt.Println("New version")
 
 }
+
+func From1() {
+	fmt.Println("Version 1.0.0")
+}
+
+func From1_2() {
+	fmt.Println("Version 1.2.0")
+}
+
+func From1_3() {
+	fmt.Println("V 1.3.0")
+}
